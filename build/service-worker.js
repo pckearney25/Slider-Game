3c1c89f7f1ea00531fd3ff2cc8460e4e1c87bcd9
Patrick Kearney <pckearney@yahoo.com>
2018-10-17 21:37:43 -0500

    package json modified

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7fea6356ce81318247525f192b1c1960.js"
+  "/Slider-Game/precache-manifest.e7ec009cfb0b695e55829be988e3eec0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Slider-Game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
