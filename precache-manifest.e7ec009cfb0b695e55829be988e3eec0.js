440838f8a9e992b5797b992664608bd8da3e8ca3
Patrick Kearney <pckearney@yahoo.com>
2018-10-17 21:36:26 -0500

    Updates

---
+++ precache-manifest.e7ec009cfb0b695e55829be988e3eec0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f2a0f7d8e0b36896425c5476fab434d",
+    "url": "/Slider-Game/static/media/freya07.8f2a0f7d.jpg"
+  },
+  {
+    "revision": "9d3c8de5d6ff4369f25b",
+    "url": "/Slider-Game/static/css/main.910a21ce.chunk.css"
+  },
+  {
+    "revision": "5624220e852419f7430f",
+    "url": "/Slider-Game/static/js/1.5624220e.chunk.js"
+  },
+  {
+    "revision": "bbf40e81e264c8bc5260",
+    "url": "/Slider-Game/static/js/runtime~main.bbf40e81.js"
+  },
+  {
+    "revision": "d9538ae1ad9d0e8d874117c9c3fadcb9",
+    "url": "/Slider-Game/static/media/freya01.d9538ae1.jpg"
+  },
+  {
+    "revision": "9f74e2a8d0d46e8147bd7b360239360a",
+    "url": "/Slider-Game/static/media/freya02.9f74e2a8.jpg"
+  },
+  {
+    "revision": "37d60c6e10034df0a1fb1dd705edc5b2",
+    "url": "/Slider-Game/static/media/freya03.37d60c6e.jpg"
+  },
+  {
+    "revision": "2e75ac36f2bff49fd73838716d21f892",
+    "url": "/Slider-Game/static/media/freya04.2e75ac36.jpg"
+  },
+  {
+    "revision": "6740ca62481eb2fc3f895d779eadab40",
+    "url": "/Slider-Game/static/media/freya05.6740ca62.jpg"
+  },
+  {
+    "revision": "1cffbec2ff13d8cfed719d91f5625f6e",
+    "url": "/Slider-Game/static/media/freya06.1cffbec2.jpg"
+  },
+  {
+    "revision": "9d3c8de5d6ff4369f25b",
+    "url": "/Slider-Game/static/js/main.9d3c8de5.chunk.js"
+  },
+  {
+    "revision": "4f36cfc356009812fcb383b9c1149c83",
+    "url": "/Slider-Game/static/media/freya08.4f36cfc3.jpg"
+  },
+  {
+    "revision": "9eb87d7ecbbe5cc69a19d4969cef1509",
+    "url": "/Slider-Game/static/media/freya09.9eb87d7e.jpg"
+  },
+  {
+    "revision": "daea715f3df93694f22f5ff40671ae87",
+    "url": "/Slider-Game/static/media/freya10.daea715f.jpg"
+  },
+  {
+    "revision": "8c02d7822d564417122aae6ceb49254c",
+    "url": "/Slider-Game/static/media/freya11.8c02d782.jpg"
+  },
+  {
+    "revision": "b2ae52ab4d9962badd68fffdc9c9f319",
+    "url": "/Slider-Game/static/media/freya12.b2ae52ab.jpg"
+  },
+  {
+    "revision": "1aafd93baa12e26219970fc6f657c732",
+    "url": "/Slider-Game/static/media/freya13.1aafd93b.jpg"
+  },
+  {
+    "revision": "c88109d1b2ed272a279ff21ea1cebc72",
+    "url": "/Slider-Game/static/media/freya14.c88109d1.jpg"
+  },
+  {
+    "revision": "52b6e61c28747a6443341b872deac647",
+    "url": "/Slider-Game/static/media/freya15.52b6e61c.jpg"
+  },
+  {
+    "revision": "41d928bcadecfbdc86ba551473e13663",
+    "url": "/Slider-Game/index.html"
+  }
+];
